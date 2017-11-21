6fa2077f4e57dc03cd77a12fa293da0fe87b3a38
github username <siobhan.p.mahoney@gmail.com>
2017-11-21 18:22:33 -0500

    work as of 622pm

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+  attr_writer :discount, :total
+  attr_reader :discount, :total
 
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(item, price, quantity=1)
+    self.total = self.total + (price * quantity)
+  end
+
+  def apply_discount
+    self.total = self.total - (self.total * (discount/100))
+  end
+end
