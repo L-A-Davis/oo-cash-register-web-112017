700ebbd32c884a23d0a6fbcd821db1bed87801cb
github username <siobhan.p.mahoney@gmail.com>
2017-11-22 00:04:21 -0500

    passed tests

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,18 +1,37 @@
 class CashRegister
-  attr_writer :discount, :total
-  attr_reader :discount, :total
-
+  attr_writer :discount, :total, :items
+  attr_reader :discount, :total, :items
 
   def initialize(discount=0)
     @total = 0
     @discount = discount
+    @items = []
   end
 
   def add_item(item, price, quantity=1)
+    @last_item = item
+    @last_price = price
     self.total = self.total + (price * quantity)
+    quantity.times do
+      @items << item
+    end
   end
 
   def apply_discount
-    self.total = self.total - (self.total * (discount/100))
+    self.total = self.total - (self.total * (discount/100.0))
+    if discount > 0
+      "After the discount, the total comes to $%g." % self.total
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total = @total - @last_price
+    @items.delete(@last_item)
   end
 end
